d3e5548e023c7886891f6972c31f672132a36c09
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-07-16 19:36:38 +0800

    update: nave

--- src/components/Nav.section/index.jsx
+++ src/components/Nav.section/index.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-        <nav className="border-b-2 border-white sticky top-0 w-full bg-gradient-to-r from-[#1D2D00] via-[#519127] to-[#b9be47] text-[2rem] text-white py-1 px-10 flex font-squada">
+        <nav className="border-b-2 border-white sticky top-0 w-full bg-gradient-to-r from-[#1D2D00] via-[#253819] to-[#686b37] text-[2rem] text-white py-1 px-10 flex font-squada">
             <div className="flex text-[3rem] ">
-                <Link to="/" className="bg-gradient-to-r from-[#9fad83] via-[#68eb11] to-[#ced811] text-transparent bg-clip-text">GHELO</Link>
+                <Link to="/" className="bg-gradient-to-b from-[#526e4280] to-[#6ceb5c] text-transparent bg-clip-text">GHELO</Link>
             </div>
             <ul className="w-full text-[2rem] flex items-center justify-center gap-16">
                 <Link>HOME</Link>
@@ -14,7 +14,7 @@ const Nav = () => {
                 <Link>CONTACT</Link>
             </ul>
             <div className="flex items-center text-[1.5rem]">
-                <Link to="/" className="bg-gradient-to-b from-[#7cda70] to-[#324e27] px-3 py-1 rounded-tl-[25%] rounded-br-[25%] whitespace-nowrap border border-white">VIEW RESUME</Link>
+                <Link to="/" className="bg-gradient-to-b from-[#8be76f] to-[#141812] px-3 py-1 rounded-tl-[25%] rounded-br-[25%] whitespace-nowrap border border-white">VIEW RESUME</Link>
             </div>
         </nav>
     )
