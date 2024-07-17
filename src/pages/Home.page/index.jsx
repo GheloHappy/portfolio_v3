fe3c55c5da24a8ad194c76c3824cb4c10ca5fa15
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-07-17 18:31:58 +0800

    update: home profile image

--- src/pages/Home.page/index.jsx
+++ src/pages/Home.page/index.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
     return (
-        <div className="w-full h-screen bg-black">
-            <h1 className="text-lg text-red-300">HOME</h1>
+        <div className="w-full h-screen bg-gradient-to-r from-[#060705] to-[#10460d] flex items-center justify-center">
+            <div className="w-[40%] flex items-center justify-center h-full">
+                <div className="w-5/6 rounded-full">
+                    <img src="https://ik.imagekit.io/ghelowee/Portfolio/img-removebg.png?updatedAt=1721212130447" alt="profile" 
+                    className="rounded-full"/>
+                </div>
+            </div>
+            <div className="w-[60%] flex items-start justify-center h-full flex-col">
+                <h1 className="font-squada text-white text-[4rem]">HI THERE! I AM</h1>
+                <div className="text-[7rem] font-squada p-0 m-0 flex bg-gradient-to-b from-[#6CFD08] to-[#495f1f] text-transparent bg-clip-text">
+                    <h1>GHELONICO G. MALIGAYA</h1>
+                </div>
+                <h1 className="font-squada text-white text-[4rem]">SOFTWARE DEVELOPER</h1>
+            </div>
         </div>
     )
 }
