69db66041da36dabd579d4b34e81b2108f0eba00
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-07-17 18:33:53 +0800

    update: change theme to red

--- src/components/Nav.section/index.jsx
+++ src/components/Nav.section/index.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-        <nav className="border-b-2 border-white sticky top-0 w-full bg-gradient-to-r from-[#1D2D00] via-[#253819] to-[#686b37] text-[2rem] text-white py-1 px-10 flex font-squada">
+        <nav className="border-b-2 border-white sticky top-0 w-full bg-gradient-to-r from-[#6e3333] via-[#381919] to-[#6b3737] text-[2rem] text-white py-1 px-10 flex font-squada">
             <div className="flex text-[3rem] ">
-                <Link to="/" className="bg-gradient-to-b from-[#526e4280] to-[#6ceb5c] text-transparent bg-clip-text">GHELO</Link>
+                <Link to="/" className="bg-gradient-to-b from-[#6e494280] to-[#eb655c] text-transparent bg-clip-text">GHELO</Link>
             </div>
             <ul className="w-full text-[2rem] flex items-center justify-center gap-16">
                 <Link>HOME</Link>
@@ -14,7 +14,7 @@ const Nav = () => {
                 <Link>CONTACT</Link>
             </ul>
             <div className="flex items-center text-[1.5rem]">
-                <Link to="/" className="bg-gradient-to-b from-[#8be76f] to-[#141812] px-3 py-1 rounded-tl-[25%] rounded-br-[25%] whitespace-nowrap border border-white">VIEW RESUME</Link>
+                <Link to="/" className="bg-gradient-to-b from-[#e76f6f] to-[#181312] px-3 py-1 rounded-tl-[25%] rounded-br-[25%] whitespace-nowrap border border-white">VIEW RESUME</Link>
             </div>
         </nav>
     )
--- src/pages/Home.page/index.jsx
+++ src/pages/Home.page/index.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div className="w-full h-screen bg-gradient-to-r from-[#060705] to-[#10460d] flex items-center justify-center">
+        <div className="w-full h-screen bg-gradient-to-r from-[#070505] to-[#460f0d] flex items-center justify-center">
             <div className="w-[40%] flex items-center justify-center h-full">
                 <div className="w-5/6 rounded-full">
                     <img src="https://ik.imagekit.io/ghelowee/Portfolio/img-removebg.png?updatedAt=1721212130447" alt="profile" 
@@ -9,7 +9,7 @@ const Home = () => {
             </div>
             <div className="w-[60%] flex items-start justify-center h-full flex-col">
                 <h1 className="font-squada text-white text-[4rem]">HI THERE! I AM</h1>
-                <div className="text-[7rem] font-squada p-0 m-0 flex bg-gradient-to-b from-[#6CFD08] to-[#495f1f] text-transparent bg-clip-text">
+                <div className="text-[7rem] font-squada p-0 m-0 flex bg-gradient-to-b from-[#fd1008] to-[#5f211f] text-transparent bg-clip-text">
                     <h1>GHELONICO G. MALIGAYA</h1>
                 </div>
                 <h1 className="font-squada text-white text-[4rem]">SOFTWARE DEVELOPER</h1>
