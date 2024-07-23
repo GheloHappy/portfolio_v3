e0e9983ae11c99e519d1ac6fbee094f2267992ae
Ghelowee <ghelonicomaligaya111@gmail.com>
2024-07-23 09:49:32 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
       <Nav />
         <Home />
         <About />
-        {/* <Routes> 
+        {/* <Routes>  
           <Route path="/" element={<Home />} />
-        </Routes> */}
+        </Routes>  */}
       </BrowserRouter>
     </>
   )
