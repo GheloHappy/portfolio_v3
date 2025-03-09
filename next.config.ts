7ec412f04dcdc6d7b0d4046baab76b4d4d2dc0ff
Ghelowee <ghelonicomaligaya111@gmail.com>
2025-03-10 05:14:20 +0800

    update: port setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    PORT: process.env.PORT,
+  },
+  poweredByHeader: false,
+  reactStrictMode: true, 
+  swcMinify: true
 };
 
 export default nextConfig;
